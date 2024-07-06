6445cf4a90cb0023f33027cfff0d7274f30ac7ad
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-07-06 16:01:15 -0300

    Inserindo dados no Banco de dados

--- Curso de Django Jefferson Lobato/Projetos/projeto_evento/projeto_eventos/models.py
+++ Curso de Django Jefferson Lobato/Projetos/projeto_evento/projeto_eventos/models.py
@@ -1,10 +1,11 @@
 from django.db import models # type: ignore
 
 class Organizador(models.Model):
-    nome = models.CharField(max_length=50, null=True, blank=True)
-    email = models.CharField(max_length=200, unique=True, null=True, blank=True)
-    ddd = models.CharField(max_length=2, null=True, blank=True)
-    telefone = models.CharField(max_length=50, null=True, blank=True)
+    nome = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=200, blank=True)
+    ddd = models.CharField(max_length=2, blank=True)
+    telefone = models.CharField(max_length=15, blank=True)
+    nick = models.CharField(max_length=30, unique=True, blank=True)
 
     def __str__(self):
         return f'{self.id} - {self.nome}'
@@ -12,11 +13,12 @@ class Organizador(models.Model):
     class Meta:
         verbose_name = 'Organizador'
         verbose_name_plural = 'Organizadores'
+        ordering = ['id']
     
 class Categoria(models.Model):
-    nome = models.CharField(max_length=30, unique=True, null=True, blank=True)
-    descricao = models.CharField(max_length=1000, null=True, blank=True)
-    ativo_inativo = models.BooleanField(null=True, blank=True)
+    nome = models.CharField(max_length=50, unique=True, blank=True)
+    descricao = models.CharField(max_length=1000, default='', blank=True)
+    ativo_inativo = models.BooleanField(default=False, blank=True)
 
     def __str__(self):
         return f'{self.id} - {self.nome}'
@@ -24,19 +26,20 @@ class Categoria(models.Model):
     class Meta:
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
+        ordering = ['id']
 
 class Evento(models.Model):
-    titulo = models.CharField(max_length=30, null=True, blank=True)
-    descricao = models.CharField(max_length=1000, null=True, blank=True)
-    rua = models.CharField(max_length=50, null=True, blank=True)
-    bairro = models.CharField(max_length=30, null=True, blank=True)
-    numero = models.IntegerField(null=True, blank=True)
-    cidade = models.CharField(max_length=30, null=True, blank=True)
-    estado = models.CharField(max_length=2, null=True, blank=True)
-    pais = models.CharField(max_length=30, null=True, blank=True)
-    data = models.DateField(null=True, blank=True)
+    titulo = models.CharField(max_length=30, unique=True, blank=True)
+    descricao = models.CharField(max_length=1000, blank=True)
+    rua = models.CharField(max_length=50, blank=True)
+    bairro = models.CharField(max_length=30, blank=True)
+    numero = models.IntegerField(blank=True)
+    cidade = models.CharField(max_length=30, blank=True)
+    estado = models.CharField(max_length=2, blank=True)
+    pais = models.CharField(max_length=30, blank=True)
+    data = models.DateField(blank=True)
     imagem = models.ImageField(upload_to='eventos_imagens/', null=True, blank=True)
-    organizador = models.ForeignKey(Organizador,on_delete=models.SET_NULL, null=True, blank=True)
+    organizador = models.ForeignKey(Organizador, on_delete=models.SET_NULL, null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
@@ -45,3 +48,4 @@ class Evento(models.Model):
     class Meta:
         verbose_name = 'Evento'
         verbose_name_plural = 'Eventos'
+        ordering = ['data']
