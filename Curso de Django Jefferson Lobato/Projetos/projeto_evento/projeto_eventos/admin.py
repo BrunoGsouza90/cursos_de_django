20a2454e30a1bd0943ebf66fda67174a13633928
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-07-06 13:00:22 -0300

    Configurações inicias prontas

--- Curso de Django Jefferson Lobato/Projetos/projeto_evento/projeto_eventos/admin.py
+++ Curso de Django Jefferson Lobato/Projetos/projeto_evento/projeto_eventos/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+from projeto_eventos.models import Evento
 
-# Register your models here.
+admin.site.register(Evento)
--- Curso de Django Jefferson Lobato/Projetos/projeto_evento/projeto_eventos/models.py
+++ Curso de Django Jefferson Lobato/Projetos/projeto_evento/projeto_eventos/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models # type: ignore
 
-# Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=1000)
+    rua = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=30)
+    numero = models.IntegerField()
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(max_length=2)
+    pais = models.CharField(max_length=30)
+    data = models.DateField()
