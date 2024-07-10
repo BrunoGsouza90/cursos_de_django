5def005372130be8b9c6f2b103cf12a6f30e734b
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-07-09 22:28:16 -0300

    Configurações finais da pagina de registro

--- Curso de Django Jefferson Lobato/Projetos/projeto_evento/users/forms.py
+++ Curso de Django Jefferson Lobato/Projetos/projeto_evento/users/forms.py
@@ -1,31 +1,31 @@
-from django.contrib.auth.models import User  # type: ignore
-from django import forms # type: ignore
-from django.core.exceptions import ValidationError # type: ignore
-
-'''
-class LoginForm(forms.Form):
-    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'placeholder': ''}))
-    password = forms.CharField(label='Senha', widget=forms.PasswordInput(attrs={'placeholder': ''}))
-'''
+from django.contrib.auth.models import User
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
-    login = forms.CharField(max_length=30, required=True)
-    senha = forms.CharField(max_length=30, widget=forms.PasswordInput(), required=True)
+    username = forms.CharField(label='Usuário')
+    password = forms.CharField(label='Senha', widget=forms.PasswordInput)
 
-    # Se eu precisar pegar dois campos ao mesmo tempo ou mais
-    #   utilizo a função clean da seguinte maneira.
-    """
-    def clean(self):
-        cleaned_data = super().clean()
-        nome = cleaned_data.get('login')
-        senha = cleaned_data.get('senha')
-    """
-
-    def clean_login(self):
-
+    def limpar_login(self):
+        
         nome = self.cleaned_data['login']
 
         if not(nome.isalnum()):
             raise ValidationError('O nome de usuário não pode ter caractere especial.')
         
         return nome
+
+
+class RegisterForm(UserCreationForm):
+    sobre_voce = forms.CharField(widget=forms.Textarea, required=False)
+    foto = forms.ImageField(required=False)
+    foto_de_capa = forms.ImageField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'sobre_voce', 'foto', 'foto_de_capa']
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=30, required=True)
+    password = forms.CharField(widget=forms.PasswordInput(), required=True)
--- Curso de Django Jefferson Lobato/Projetos/projeto_evento/users/views.py
+++ Curso de Django Jefferson Lobato/Projetos/projeto_evento/users/views.py
@@ -1,10 +1,9 @@
-from django.urls import reverse # type: ignore
-from django.shortcuts import render, redirect, HttpResponseRedirect # type: ignore
-from django.contrib.auth import authenticate, login, logout # type: ignore
-from django.contrib.auth.decorators import login_required # type: ignore
-from .forms import LoginForm
-from projeto_eventos.models import Categoria
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, LoginForm  # Importe os formulários RegisterForm e LoginForm
+from projeto_eventos.models import Categoria
 
 def login_view(request):
     categorias = Categoria.objects.all()
@@ -18,10 +17,8 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                # Redirecione para a página desejada após o login
-                return HttpResponseRedirect(reverse('home'))
+                return redirect('home')  # Redirecionar para a página desejada após o login
             else:
-                # Trate o erro de credenciais inválidas aqui
                 error = "Login ou senha incorretos. Por favor, tente novamente."
                 context['form'] = form
                 context['error'] = error
@@ -29,28 +26,49 @@ def login_view(request):
             context['form'] = form
     else:
         form = LoginForm()
-        context['form'] = form  # Defina context mesmo fora do POST
+        context['form'] = form
 
     return render(request, 'users/login.html', context)
 
+@login_required
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse('login'))
+    return redirect('login')
 
 def register(request):
-    """Faz o cadastro de um novo usuário."""
     if request.method == "POST":
-        form = UserCreationForm(data=request.POST)
-
+        form = RegisterForm(request.POST, request.FILES)
         if form.is_valid():
-            new_user = form.save()
-            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            username = form.cleaned_data['username']
+            sobre_voce = form.cleaned_data.get('sobre_voce', '')
+            foto = form.cleaned_data.get('foto', None)
+            foto_de_capa = form.cleaned_data.get('foto_de_capa', None)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+
+            new_user = form.save(commit=False)
+            new_user.email = email
+            new_user.first_name = first_name
+            new_user.last_name = last_name
+
+            if sobre_voce:
+                new_user.sobre_voce = sobre_voce
+            if foto:
+                new_user.foto = foto
+            if foto_de_capa:
+                new_user.foto_de_capa = foto_de_capa
+
+            new_user.save()
+
+            authenticated_user = authenticate(username=username, password=password1)
             login(request, authenticated_user)
-            if authenticated_user:
-                return HttpResponseRedirect(reverse('home'))
-    
+
+            return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     context = {"form": form}
     return render(request, 'users/register.html', context)
