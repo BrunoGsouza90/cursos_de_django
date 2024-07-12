09c1953e1ce58a6162b157ce1cd4f9ba362246e1
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-07-12 08:59:35 -0300

    Finalização da construção funcional do site

--- Curso de Django Jefferson Lobato/Projetos/projeto_evento/projeto_eventos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin # type: ignore
-from projeto_eventos.models import Evento, Categoria, Contato
-
-admin.site.register(Evento)
-admin.site.register(Categoria)
-admin.site.register(Contato)
