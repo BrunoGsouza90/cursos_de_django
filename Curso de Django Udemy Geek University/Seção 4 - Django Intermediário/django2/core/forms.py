36144ff953812eb05f098a5d8e69c06afef990bf
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-08-02 09:23:20 -0300

    Geek University - Início da Seção 4 - Intermediário

---
+++ Curso de Django Udemy Geek University/Seção 4 - Django Intermediário/django2/core/forms.py
@@ -0,0 +1,68 @@
+'''
+    Será nesse arquivo que criaremos os nossos formulários.
+'''
+
+'''
+    Importação da biblioteca de formulários do Django.
+'''
+from django import forms
+
+'''
+    Importação da biblioteca do Django responsável pelo envio
+        de emails.
+'''
+from django.core.mail.message import EmailMessage
+
+'''
+    Aqui estamos criando uma classe de formulário Django.
+'''
+class ContatoForm(forms.Form):
+
+    '''
+        Aqui estamos definando os campos do nosso formulário.
+    '''
+    nome = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='Email')
+    assunto = forms.CharField(label='Assunto')
+
+    '''
+        Nesse campo em específico estamos importando um widget
+            que determinará qual tipo de input esse campo será.
+        Nesse caso específicamos que o campo será um campo de
+            texto.
+        rows = Número de linhas visíveis.
+        cols = Número de colunas visíveis.
+    '''
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea(
+            attrs={          
+            'rows': 4,
+            'cols': 40
+            }
+    ))
+
+    '''
+        Função responsável pelo envio de emails no Django.
+    '''
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        conteudo = f'Nome: {nome} \n E-mail: {email} \n Assunto: {assunto} \n Mensagem: {mensagem}'
+        
+        '''
+            Aqui estamos preparando o email.
+        '''
+        mail = EmailMessage(
+            subject='E-mail enviado pelo sistema django2',
+            body=conteudo,
+            from_email='contato@seudominio.com',
+            to=['contato@seudominio.com.br', 'outro@seuemail.com'],
+            headers={'Reply-To': email}
+        )
+
+        '''
+            Aqui fazemos o envio do email.
+        '''
+        mail.send()
---
+++ Curso de Django Udemy Geek University/Seção 4 - Django Intermediário/django2/django2/settings.py
@@ -0,0 +1,123 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-qrqm5(h(7p-s-#rc*vh)m%c&&4#agcfetdnz7+brn61d4%1*41'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'core',
+    'bootstrap4',
+    'stdimage',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    # 'whitenoise.middleWare.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'django2.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'django2.wsgi.application'
+
+
+'''
+    Configuração do Banco de Dados MySQL.
+    Nesta aplicação estamos utilizando as variáveis de ambiente
+        para armanezar os dados sigilosos.
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DJANGO_DB_NAME'),
+        'USER': os.getenv('DJANGO_DB_USER'),
+        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD'),
+        'HOST': os.getenv('DJANGO_DB_HOST'),
+        'PORT': os.getenv('DJANGO_DB_PORT'),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'''
+    Configuração de Simulação de Email do Django.
+    Aqui o Django armazena o email até que tenhamos um servidor
+        de email.
+'''
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+'''
+    Quando estiver um servidor de email será usado essa
+        configuração:
+    
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = 'seu_email@gmail.com'
+    EMAIL_HOST_PASSWORD = 'sua_senha'
+'''
